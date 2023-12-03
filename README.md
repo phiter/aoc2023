60e65bff490ee02f60210e7fdd6dea7dbda5f6f1
Phiter Fernandes <phiterfernandes@gmail.com>
2023-12-03 03:35:46 -0300

    add day3 solution

--- README.md
+++ README.md
@@ -12,8 +12,8 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -69,23 +69,23 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.52ms
-Time part 2: 1.718ms
-Both parts: 2.238ms
+Time part 1: 0.829ms
+Time part 2: 31.818ms
+Both parts: 32.647ms
 ```
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.397ms
+Time part 2: 0.39ms
+Both parts: 0.786ms
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 4.664ms
+Time part 2: 2.433ms
+Both parts: 7.097ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 2.238ms
+Total stars: 6/50
+Total time: 40.53ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day03/index.ts
@@ -0,0 +1,117 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput.split('\n').map(l => l.trim());
+
+const checkOverlap = (match1: RegExpMatchArray, match2: RegExpMatchArray) => {
+  const startOverlap = match1.index! + 1 >= match2.index!;
+  const endOverlap = match1.index! <= (match2.index! + match2[0].length);
+
+  return startOverlap && endOverlap;
+}
+
+const symbolRegex = new RegExp('[^.\\dA-Za-z]', 'g');
+const numbersRegex = new RegExp('[0-9]+', 'g');
+
+const checkNeighbors = (match: RegExpMatchArray, neighbors: string[]) => {
+  for (const neighbor of neighbors) {
+    if (!neighbor) continue;
+    const symbols = [...neighbor.matchAll(symbolRegex)];
+    if (symbols.some(symbol => checkOverlap(symbol, match))) {
+      return true;
+    };
+  }
+
+  return false;
+};
+
+const part1 = (rawInput: string) => {
+  const lines = parseInput(rawInput);
+  let total = 0;
+  for (const idx in lines) {
+    const index = Number(idx);
+    const line = lines[index];
+    const matches = [...line.matchAll(numbersRegex)];
+    for (const match of matches) {
+      if (checkNeighbors(match, [lines[index - 1], line, lines[index + 1]])) {
+        total += Number(match[0]);
+      }
+    }
+  }
+  return total;
+};
+
+const getCogParts = (cog: RegExpMatchArray, neighbors: string[]) => {
+  const neighboringNumbers: number[] = [];
+  for (const neighbor of neighbors) {
+    if (!neighbor) continue;
+    const numbers = [...neighbor.matchAll(numbersRegex)];
+    for (const number of numbers) {
+      if (checkOverlap(cog, number)) {
+        neighboringNumbers.push(Number(number[0]));
+      }
+    }
+  }
+
+  return neighboringNumbers;
+};
+
+const cogRegex = new RegExp('\\*', 'g');
+const part2 = (rawInput: string) => {
+  const lines = parseInput(rawInput);
+  let total = 0;
+  for (const idx in lines) {
+    const index = Number(idx);
+    const line = lines[index];
+
+    const cogs = [...line.matchAll(cogRegex)];
+    for (const cog of cogs) {
+      const parts = getCogParts(cog, [lines[index - 1], line, lines[index + 1]]);
+      if (parts.length === 2) {
+        total += parts[0] * parts[1];
+      }
+    }
+  }
+
+  return total;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `467..114..
+                ...*......
+                ..35..633.
+                ......#...
+                617*......
+                .....+.58.
+                ..592.....
+                ......755.
+                ...$.*....
+                .664.598..`,
+        expected: 4361,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `467..114..
+                ...*......
+                ..35..633.
+                ......#...
+                617*......
+                .....+.58.
+                ..592.....
+                ......755.
+                ...$.*....
+                .664.598..`,
+        expected: 467835,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
