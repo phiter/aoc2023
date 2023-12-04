a66a95f1d84cfeb65c67f330eb2dba3f2c9e0baa
Phiter Fernandes <phiterfernandes@gmail.com>
2023-12-04 20:09:48 -0300

    add day04 solution

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -83,16 +83,16 @@ Both parts: 0.786ms
 
 ```
 Day 03
-Time part 1: 4.664ms
-Time part 2: 2.433ms
-Both parts: 7.097ms
+Time part 1: 4.449ms
+Time part 2: 2.273ms
+Both parts: 6.722ms
 ```
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.315ms
+Time part 2: 1.065ms
+Both parts: 2.38ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 40.53ms
+Total stars: 8/50
+Total time: 42.535ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day04/index.ts
@@ -0,0 +1,79 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput.split('\n').map(l => l.trim());
+const sumArrayItems = (array: number[]) => array.reduce((t, i) => t + i, 0);
+
+const parseLineMatches = (line: string) => {
+  const [winningNumbers, numbersYouHave] = line.replace(/Card\s+[0-9]+: /, '').split('|').map(l => l.trim().split(/\s+/g));
+  const matchingNumbers = [...new Set(numbersYouHave.filter(number => winningNumbers.includes(number)))];
+  return matchingNumbers.length;
+}
+
+const part1 = (rawInput: string) => {
+  const lines = parseInput(rawInput);
+  let total = 0;
+  for (const line of lines) {
+    const matchingNumbers = parseLineMatches(line);
+    if (matchingNumbers === 0) continue;
+
+    total += Math.pow(2, matchingNumbers - 1);
+  }
+  return total;
+};
+
+/**
+ * Stores all solved cards, backwards.
+ * Cards with 0 wins resolve to 1 (the card itself)
+ * Cards with more than 1 win will take the next x resolved cards and sum them.
+ */
+const part2 = (rawInput: string) => {
+  const lines = parseInput(rawInput);
+
+  let total = 0;
+  const resolvedLines: number[] = [];
+  const length = lines.length;
+  for (let index = length - 1; index >= 0; index--) {
+    const matches = parseLineMatches(lines[index]);
+    const inverseIndex = length - index - 1;
+
+    // Get resolved values of clones
+    const slice = resolvedLines.slice(inverseIndex - matches, inverseIndex);
+    const resolution = sumArrayItems(slice) + 1;
+    resolvedLines.push(resolution);
+    total += resolution;
+  }
+  return total;
+}
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 13,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 30,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
