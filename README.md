f3bd62e3b768585b436070b502fef4078222e89d
Phiter Fernandes <phiterfernandes@gmail.com>
2023-12-08 01:04:46 -0300

    add day7 solution

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -111,9 +111,9 @@ Both parts: 14.018ms
 
 ```
 Day 07
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 4.29ms
+Time part 2: 4.019ms
+Both parts: 8.309ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 12/50
-Total time: 17325333.36ms
+Total stars: 14/50
+Total time: 17325341.67ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day07/index.ts
@@ -0,0 +1,133 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput.split('\n');
+
+const cardOrder = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'] as const;
+const cardRankMap: Record<string, number> = cardOrder.reduce((acc, c, index) => ({ ...acc, [c]: cardOrder.length - index }), {});
+const jokerCardRankMap: Record<string, number> = {
+  ...cardRankMap,
+  J: -1
+}
+
+type Hand = { cards: string[], bid: number, map: Entry[], rank: number };
+
+const parseHand = (line: string, withJoker = false): Hand => {
+  const parts = line.split(' ');
+
+  const cards = parts[0].split('');
+  const bid = Number(parts[1]);
+  const map = mapHandCards(cards);
+
+  if (withJoker) {
+    const jokerIndex = map.findIndex(entry => entry[0] === "J");
+
+    if (jokerIndex > -1 && map.length > 1) {
+      if (jokerIndex === 0) {
+        map[1][1] += map[0][1];
+        // Swap J and biggest card positions
+        [map[0][0], map[1][0]] = [map[1][0], map[0][0]];
+      } else {
+        map[0][1] += map[jokerIndex][1];
+      }
+      map.splice(jokerIndex, 1);
+    }
+  }
+
+  const rank = map[0][1] - map.length - 1;
+  return {
+    cards,
+    bid,
+    map,
+    rank
+  }
+};
+
+
+type Entry = [string, number];
+
+const mapHandCards = (cards: string[]) => {
+  const cardMap: Map<string, number> = new Map();
+  for (const index in cards) {
+    const card = cards[index];
+    if (!cardMap.has(card)) {
+      cardMap.set(card, 0);
+    }
+
+    cardMap.set(card, cardMap.get(card)! + 1);
+  }
+
+  return [...cardMap.entries()].sort((a, b) => b[1] - a[1]);
+}
+
+const sortHands = (handA: Hand, handB: Hand, withJoker = false) => {
+  if (handA.rank !== handB.rank) {
+    return handA.rank - handB.rank;
+  }
+
+  for (let i = 0; i < 5; i++) {
+    const rankA = withJoker ? jokerCardRankMap[handA.cards[i]] : cardRankMap[handA.cards[i]];
+    const rankB = withJoker ? jokerCardRankMap[handB.cards[i]] : cardRankMap[handB.cards[i]];
+    if (rankA !== rankB) {
+      return rankA - rankB;
+    }
+  }
+
+  return 0;
+}
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const hands = input.map(l => parseHand(l));
+  const sorted = hands.sort(sortHands);
+  let acc = 0;
+  for (const handIndex in sorted) {
+    const index = Number(handIndex);
+    const hand = sorted[handIndex];
+    acc += hand.bid * (index + 1);
+  }
+  return acc;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const hands = input.map((l) => parseHand(l, true));
+  const sorted = hands.sort((a, b) => sortHands(a, b, true));
+  let acc = 0;
+  for (const handIndex in sorted) {
+    const index = Number(handIndex);
+    const hand = sorted[handIndex];
+    acc += hand.bid * (index + 1);
+  }
+  return acc;
+};
+
+const input = `
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+`;
+
+run({
+  part1: {
+    tests: [
+      {
+        input,
+        expected: 6440,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input,
+        expected: 5905,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
